10aca5dece337a02b2112ddb1396893e4728d9c3
Daniel Kenyon-Jones <github@dlkj.co.uk>
2024-03-08 14:32:09 +0000

    move to release embassy crates

--- rust/cross/app/src/bin/app.rs
+++ rust/cross/app/src/bin/app.rs
@@ -13,7 +13,7 @@ use embassy_net::{Config, IpEndpoint, Stack, StackResources};
 use embassy_rp::bind_interrupts;
 use embassy_rp::clocks::RoscRng;
 use embassy_rp::gpio::{Level, Output};
-use embassy_rp::peripherals::{DMA_CH0, PIO0};
+use embassy_rp::peripherals::{DMA_CH0, PIN_23, PIN_25, PIO0};
 use embassy_rp::pio::{InterruptHandler, Pio};
 use embassy_rp::rtc::{DateTime, Rtc};
 use embassy_time::{Duration, Timer};
@@ -35,7 +35,11 @@ bind_interrupts!(struct Irqs {
 
 #[embassy_executor::task]
 async fn wifi_task(
-    runner: cyw43::Runner<'static, Output<'static>, PioSpi<'static, PIO0, 0, DMA_CH0>>,
+    runner: cyw43::Runner<
+        'static,
+        Output<'static, PIN_23>,
+        PioSpi<'static, PIN_25, PIO0, 0, DMA_CH0>,
+    >,
 ) -> ! {
     runner.run().await
 }
